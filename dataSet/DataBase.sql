6b60faedf45c27149a1d54c7437d49ebecc90ae3
LuisZambranoRamirez <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-29 11:51:20 -0500

    dataset

---
+++ dataSet/DataBase.sql
@@ -0,0 +1,54 @@
+
+CREATE TABLE song (
+    song_name VARCHAR(255),
+    song_artist VARCHAR(255),
+    song_popularity DOUBLE PRECISION,
+    danceability DOUBLE PRECISION,
+    energy DOUBLE PRECISION,
+    key DOUBLE PRECISION,
+    loudness DOUBLE PRECISION,
+    mode DOUBLE PRECISION,
+    speechiness DOUBLE PRECISION,
+    acousticness DOUBLE PRECISION,
+    instrumentalness DOUBLE PRECISION,
+    liveness DOUBLE PRECISION,
+    valence DOUBLE PRECISION,
+    tempo DOUBLE PRECISION,
+    duration_ms DOUBLE PRECISION,
+
+    PRIMARY KEY (song_name, song_artist)
+);
+
+CREATE TABLE genre (
+    genre VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE subgenre (
+    subgenre VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE song_genre_map (
+    song_name VARCHAR(255),
+    song_artist VARCHAR(255),
+    genre VARCHAR(100),
+
+    PRIMARY KEY (song_name, song_artist, genre),
+
+    FOREIGN KEY (song_name, song_artist) 
+        REFERENCES song(song_name, song_artist) ON DELETE CASCADE,
+    FOREIGN KEY (genre) 
+        REFERENCES genre(genre) ON DELETE CASCADE
+);
+
+CREATE TABLE song_subgenre_map (
+    song_name VARCHAR(255),
+    song_artist VARCHAR(255),
+    subgenre VARCHAR(100),
+
+    PRIMARY KEY (song_name, song_artist, subgenre),
+
+    FOREIGN KEY (song_name, song_artist) 
+        REFERENCES song(song_name, song_artist) ON DELETE CASCADE,  
+    FOREIGN KEY (subgenre) 
+        REFERENCES subgenre(subgenre) ON DELETE CASCADE
+);
