63d9b8cac86d59e36bb7673073df95ad35de154b
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-30 23:05:13 -0500

    chore: promteo init2.0

--- prometeo/src/main/java/com/prometeo/application/PrometeoApplication.java
+++ prometeo/src/main/java/com/prometeo/application/PrometeoApplication.java
@@ -1,4 +1,4 @@
-package com.prometeo.prometeo;
+package com.prometeo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
