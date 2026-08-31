827e0b139ff578a7e5e99f8dd81a7563e4a649a8
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-30 22:59:28 -0500

    chore: promteo init

---
+++ prometeo/prometeo/src/main/java/com/prometeo/prometeo/PrometeoApplication.java
@@ -0,0 +1,13 @@
+package com.prometeo.prometeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometeoApplication.class, args);
+	}
+
+}
---
+++ prometeo/prometeo/src/test/java/com/prometeo/prometeo/PrometeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometeo.prometeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
